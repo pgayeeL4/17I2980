2fdcdc1ce9858f4a851f34a45535db5234ff770b
B. Ricks <bricks@unomaha.edu>
2017-02-28 10:15:57 -0600

    Cleaning up the code some for color reduction.

---
+++ ColorReduction/src/colorReduction/MoreKernels.java
@@ -0,0 +1,190 @@
+package colorReduction;
+
+import java.io.FileInputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MoreKernels extends Application {
+    
+    static Image inputImageJfx;
+    static MyImage outputImageJfx;
+    
+    static List<MyImage> undoStack = new ArrayList<MyImage>();
+    static List<MyImage> redoStack = new ArrayList<MyImage>();
+    
+
+    public static void main(String[] args) throws Exception{
+        
+        inputImageJfx = new Image(new FileInputStream(System.getProperty("user.home") + "/Desktop/photo.jpg"));;
+        
+        outputImageJfx = new MyImage((int)inputImageJfx.getWidth(), (int)inputImageJfx.getHeight());;
+        
+        outputImageJfx.copyFrom(inputImageJfx);
+        
+        
+        
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        //ImageView inputImageView = new ImageView(inputImageJfx);
+        ImageView outputImageView = new ImageView(outputImageJfx);
+      
+        VBox menuPane = new VBox();
+        
+        Button undoButton = new Button("Undo");
+        undoButton.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                
+                
+                if(undoStack.size() > 0)
+                {
+                    MyImage previousImage = undoStack.get(undoStack.size() - 1);
+                    undoStack.remove(undoStack.size() - 1);
+                    
+                    redoStack.add(outputImageJfx);
+                    
+                    outputImageJfx = previousImage;
+                            
+                            
+                    outputImageView.setImage(outputImageJfx);
+                    
+                    
+                }
+                
+            }
+        });
+        menuPane.getChildren().add(undoButton);
+        
+        Button redoButton = new Button("Redo");
+        redoButton.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                if(redoStack.size() > 0)
+                {
+                    MyImage previousImage = redoStack.get(redoStack.size() - 1);
+                    redoStack.remove(redoStack.size() - 1);
+                    
+                    undoStack.add(outputImageJfx);
+                    
+                    outputImageJfx = previousImage;
+                            
+                            
+                    outputImageView.setImage(outputImageJfx);
+                    
+                    
+                }
+            }
+        });
+        menuPane.getChildren().add(redoButton);
+        
+        menuPane.getChildren().add(new Separator());
+        
+        
+        for(Method method : outputImageJfx.getClass().getDeclaredMethods())
+        {
+            if (!Modifier.isPublic(method.getModifiers())) {
+                continue;
+            }
+            
+            
+            Button button = new Button(method.getName());
+            button.setOnAction(
+                    new EventHandler<ActionEvent>(){
+                    @Override
+                    public void handle(ActionEvent event) {
+                        try {  
+                            
+                            MyImage newImage = new MyImage((int)inputImageJfx.getWidth(), (int)inputImageJfx.getHeight());
+                            
+                            newImage.copyFrom(outputImageJfx);
+                            
+                            undoStack.add(outputImageJfx);
+                            
+                            redoStack.clear();
+                            
+                            outputImageJfx = newImage;
+                            
+                            
+                            method.invoke(outputImageJfx, new Object[]{1});
+                            
+                            outputImageView.setImage(outputImageJfx);
+                            
+                        } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
+                            Logger.getLogger(MoreKernels.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    }
+
+                }
+            );
+            
+            
+            menuPane.getChildren().add(button);
+        }
+        
+        menuPane.getChildren().add(new Separator());
+        
+        Button reduceButton = new Button("Reduce Colors");
+        reduceButton.setOnAction(new EventHandler<ActionEvent>(){
+
+            @Override
+            public void handle(ActionEvent event) {
+                            
+                MyImage newImage = new MyImage((int)inputImageJfx.getWidth(), (int)inputImageJfx.getHeight());
+
+                newImage.copyFrom(outputImageJfx);
+
+                undoStack.add(outputImageJfx);
+
+                redoStack.clear();
+
+                outputImageJfx = newImage;
+
+
+                outputImageJfx.reduceColors(3);
+
+                outputImageView.setImage(outputImageJfx);
+                    
+            }
+            
+        
+        });
+        
+        menuPane.getChildren().add(reduceButton);
+        
+        
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(10, 10, 10, 10));
+        root.setStyle("-fx-background-color: DAE6F3;");
+        root.setCenter(outputImageView);
+        
+        
+        root.setLeft(menuPane);
+        
+        Scene scene = new Scene(root, inputImageJfx.getWidth() + 400, inputImageJfx.getHeight() + 20 );
+        
+        primaryStage.setTitle("Duplicates");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ ColorReduction/src/colorReduction/MyImage.java
@@ -0,0 +1,265 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colorReduction;
+
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author unouser
+ */
+public class MyImage extends WritableImage{
+
+    public MyImage(int i, int i0) {
+        super(i, i0);
+    }
+    
+    public void copyFrom(Image inImage){
+        for(int y = 0; y < this.getHeight() ;  y++)
+        {
+            for(int x = 0; x < this.getWidth() ; x++)
+            {
+                this.getPixelWriter().setColor(x, y,  inImage.getPixelReader().getColor(x, y));
+            }
+        }
+    }
+    
+    public void save(){
+        RenderedImage renderedImage = SwingFXUtils.fromFXImage(this, null);
+        try {
+            ImageIO.write(renderedImage, "PNG", new File(System.getProperty("user.home") + "/Desktop/newPhoto.png") );
+        } catch (IOException ex) {
+            Logger.getLogger(MyImage.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+    private void applyKernel(float[][] kernel, int w)
+    {
+        Color[][] newColors = new Color[(int)this.getHeight()][(int)this.getWidth()];
+        
+        for(int y = 0; y < this.getHeight() ;  y++)
+        {
+            for(int x = 0; x < this.getWidth() ; x++)
+            {
+                
+                Color myColor = this.getPixelReader().getColor(x, y );
+                if(myColor.getOpacity() == 0)
+                {
+                    newColors[y][x] = Color.TRANSPARENT;
+                    continue;
+                }
+                
+                
+                float sumR= 0;
+                float sumG= 0;
+                float sumB= 0;
+                
+                float power = 0;
+                
+                for(int i = -1 * w; i <= 1 * w; i++)
+                {
+                    for(int j = -1 * w; j <= 1 * w; j++)
+                    {
+                        if(this.doesPixelExist(x + i, y + j))
+                        {
+                            Color currentPixel = this.getPixelReader().getColor(x + i, y + j);
+                            float kernelValue = kernel[j + w][i + w];
+                            sumR += currentPixel.getRed() * kernelValue;
+                            sumG += currentPixel.getGreen()* kernelValue;
+                            sumB += currentPixel.getBlue()* kernelValue;
+                            power +=kernelValue;
+                        }
+                        
+                    }
+                }
+                
+                sumR /= power;
+                sumG /= power;
+                sumB /= power;
+                
+                
+                
+                newColors[y][x] = new Color(
+                        clampAbs(sumR), 
+                        clampAbs(sumG), 
+                        clampAbs(sumB), 
+                        1.0f );
+            }
+        }
+        
+        for(int y = 0; y < this.getHeight() ;  y++)
+        {
+            for(int x = 0; x < this.getWidth() ; x++)
+            {
+                this.getPixelWriter().setColor(x, y, newColors[y][x]);
+            }
+        }
+    }
+    
+    public void blur(int w) {
+        
+        float[][] blurKernel = new float[2*w + 1][2 * w + 1];
+        
+        for(int y = 0; y < 2 * w + 1 ;  y++)
+        {
+            for(int x = 0; x < 2 * w+ 1 ; x++)
+            {
+                blurKernel[y][x] = 1;
+            }
+        }
+        
+        this.applyKernel(blurKernel, w);
+        
+    }
+    
+    public void doNothing(int w) {
+        
+        float[][] doNothingKernel = new float[2*w + 1][2 * w + 1];
+        
+        for(int y = 0; y < 2 * w + 1 ;  y++)
+        {
+            for(int x = 0; x < 2 * w+ 1 ; x++)
+            {
+                doNothingKernel[y][x] = 0;
+                if(y == w && x == w)
+                    doNothingKernel[y][x] = 1;
+            }
+        }
+        
+        this.applyKernel(doNothingKernel, w);
+    } 
+    
+        
+    public void magic(int w) {
+        
+        float[][] magicKernel = new float[2*w + 1][2 * w + 1];
+        
+        for(int y = 0; y < 2 * w + 1 ;  y++)
+        {
+            for(int x = 0; x < 2 * w+ 1 ; x++)
+            {
+                magicKernel[y][x] = 0;
+               
+            }
+        }
+        
+        magicKernel[0][0] = 1;
+        magicKernel[2*w][2*w] = 1;
+        
+        this.applyKernel(magicKernel, w);
+        
+    }
+    
+    public void edgeDetector(int i) {
+        
+        int w = 1; ///3x3 kernel
+        float[][] magicKernel = new float[2*w + 1][2 * w + 1];
+        
+        for(int y = 0; y < 2 * w + 1 ;  y++)
+        {
+            for(int x = 0; x < 2 * w+ 1 ; x++)
+            {
+                magicKernel[y][x] = 0;
+               
+            }
+        }
+        
+        magicKernel[w][w] = 4;
+        magicKernel[w][0] = -1;
+        magicKernel[0][w] = -1;
+        magicKernel[w][2] = -1;
+        magicKernel[2][w] = -1;
+        
+        
+        
+        this.applyKernel(magicKernel, w);
+        
+    }
+    
+    public void sharpen(int i) {
+        
+        int w = 1; ///3x3 kernel
+        float[][] magicKernel = new float[2*w + 1][2 * w + 1];
+        
+        for(int y = 0; y < 2 * w + 1 ;  y++)
+        {
+            for(int x = 0; x < 2 * w+ 1 ; x++)
+            {
+                magicKernel[y][x] = 0;
+               
+            }
+        }
+        
+        magicKernel[w][w] = 5;
+        magicKernel[w][0] = -1;
+        magicKernel[0][w] = -1;
+        magicKernel[w][2] = -1;
+        magicKernel[2][w] = -1;
+        
+        
+        
+        this.applyKernel(magicKernel, w);
+        
+    }
+    
+    public void reduceColors(int finalColorCount)
+    {
+        if(finalColorCount <= 0) return; //Doesn't make sense without a positive number
+        
+        List<Color> finalColors = new ArrayList<Color>();
+        
+        for(int i = 0; i < finalColorCount; i++)
+        {
+            double red = 0;
+            double green = 0;
+            double blue = 0;
+            
+            //Now assign values based on some heuristic
+            red = Math.random();
+            green = Math.random();
+            blue = Math.random();
+            
+            Color randomColor = new Color(red,green,blue,1);
+            finalColors.add(randomColor);
+        }
+        
+        
+        
+        
+        
+        
+    }
+
+    private boolean doesPixelExist(int x, int y) {
+        return x < this.getWidth() && x >= 0 && y < this.getHeight() && y >= 0;
+    }
+    
+    private float clampAbs(float f){
+        float toReturn = Math.abs(f);
+        
+        if(toReturn < 0)
+            return 0;
+        if(toReturn > 1)
+            return 1;
+        
+        return toReturn;
+    }
+    
+    
+}
